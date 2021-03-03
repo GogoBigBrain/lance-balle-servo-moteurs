8effa6ff3f6a2234e3e9aa5f0810595d999af3bc
Subscribe2Gordulf <79945148+GogoBigBrain@users.noreply.github.com>
2021-03-03 16:30:00 -0500

    Version clean comme si évalué

--- main.ts
+++ main.ts
@@ -1,39 +1,50 @@
-function envoyer () {
-    servos.P0.setAngle(93)
-}
-function arrêter_roues () {
-	
-}
-input.onButtonPressed(Button.A, function () {
+/**
+ * Mode d'emploi
+ * 
+ * 4 modes à changer avec A+B
+ * 
+ * MODE1: Balles
+ * 
+ * MODE2: Direction horizontale
+ * 
+ * MODE3: Direction vertical
+ * 
+ * MODE4: Propulsion
+ * 
+ * Merci de me laisser vos commentaires
+ * 
+ * -Thomas
+ */
+function ActionA () {
     if (mode == 1) {
-        basic.showNumber(1)
-        Recharger()
-        basic.pause(750)
-        envoyer()
-        basic.pause(750)
+        MA1()
     }
     if (mode == 2) {
-        basic.showNumber(2)
-        angle_p1 += -20
-        servos.P1.setAngle(angle_p1)
-        if (angle_p1 < 0) {
-            angle_p1 += 20
-        }
+        MA2()
     }
     if (mode == 3) {
-        basic.showNumber(3)
-        angle_p2 += -20
-        servos.P2.setAngle(angle_p2)
+        MA3()
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    mode += 1
-    basic.showNumber(mode)
     if (mode == 4) {
-        mode = 1
-        basic.showNumber(1)
+    	
     }
-})
+}
+function MB4 () {
+	
+}
+function MA2 () {
+    basic.showNumber(2)
+    angle_p1 += -20
+    servos.P1.setAngle(angle_p1)
+    if (angle_p1 < 0) {
+        angle_p1 += 20
+    }
+}
+/**
+ * Fonctions
+ * 
+ * système de chargement ------------>
+ */
 /**
  * Fonctions
  * 
@@ -41,34 +52,64 @@ input.onButtonPressed(Button.AB, function () {
  * 
  * pas encore dévelopé
  */
+function envoyer () {
+    servos.P0.setAngle(93)
+}
+function Toujours () {
+    mode = Math.constrain(mode, 1, 5)
+}
+function arrêter_roues () {
+	
+}
+function Démarage () {
+    mode = 1
+    angle_p1 = 90
+    angle_p2 = 0
+    servos.P0.setAngle(32)
+    servos.P1.setAngle(90)
+    servos.P1.setRange(0, 180)
+    servos.P2.setRange(0, 45)
+}
+input.onButtonPressed(Button.A, function () {
+    ActionA()
+})
+function MA1 () {
+    basic.showNumber(1)
+    Recharger()
+    basic.pause(750)
+    envoyer()
+    basic.pause(750)
+}
+input.onButtonPressed(Button.AB, function () {
+    AB()
+})
+function MB2 () {
+    basic.showNumber(2)
+    angle_p1 += 20
+    servos.P1.setAngle(angle_p1)
+    if (angle_p1 > 180) {
+        angle_p1 += -20
+    }
+}
+function MA3 () {
+    basic.showNumber(3)
+    angle_p2 += -20
+    servos.P2.setAngle(angle_p2)
+}
 function partir_roues () {
 	
 }
+function AB () {
+    mode += 1
+    basic.showNumber(mode)
+    if (mode == 5) {
+        mode = 1
+        basic.showNumber(mode)
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    if (mode == 1) {
-        basic.showNumber(1)
-        automatique()
-        basic.pause(750)
-    }
-    if (mode == 2) {
-        basic.showNumber(2)
-        angle_p1 += 20
-        servos.P1.setAngle(angle_p1)
-        if (angle_p1 > 180) {
-            angle_p1 += -20
-        }
-    }
-    if (mode == 3) {
-        basic.showNumber(3)
-        angle_p2 += 20
-        servos.P2.setAngle(angle_p2)
-    }
+    ActionB()
 })
-/**
- * Fonctions
- * 
- * système de chargement ------------>
- */
 function Recharger () {
     servos.P0.setAngle(32)
 }
@@ -78,6 +119,9 @@ function semiauto () {
     envoyer()
     basic.pause(750)
 }
+function MA4 () {
+	
+}
 function automatique () {
     for (let index = 0; index < 5; index++) {
         Recharger()
@@ -86,16 +130,34 @@ function automatique () {
         basic.pause(750)
     }
 }
+function MB1 () {
+    basic.showNumber(1)
+    automatique()
+    basic.pause(750)
+}
+function MB3 () {
+    basic.showNumber(3)
+    angle_p2 += 20
+    servos.P2.setAngle(angle_p2)
+}
+function ActionB () {
+    if (mode == 1) {
+        MB1()
+    }
+    if (mode == 2) {
+        MB2()
+    }
+    if (mode == 3) {
+        MB3()
+    }
+    if (mode == 4) {
+    	
+    }
+}
 let angle_p2 = 0
 let angle_p1 = 0
 let mode = 0
-mode = 1
-angle_p1 = 90
-angle_p2 = 0
-servos.P0.setAngle(32)
-servos.P1.setAngle(90)
-servos.P1.setRange(0, 180)
-servos.P2.setRange(0, 45)
+Démarage()
 basic.forever(function () {
-    mode = Math.constrain(mode, 1, 4)
+    Toujours()
 })
